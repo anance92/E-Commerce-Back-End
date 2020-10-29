0cc6fdee9e7c95db1f4228762cb7840be3d22d4c
Alexandra <anance5@murraystate.edu>
2020-10-29 16:24:37 -0500

    create associations between models

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,26 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+
+  Product.findAll({}).then(products => {
+    res.json(products);
+  })
+
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+  Product.findOne({
+    where : {
+      id : req.params.id
+    }
+  }).then(product => {
+    res.json(product);
+  })
+
 });
 
 // create new product
@@ -91,6 +105,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+
+  Product.destroy({
+    where : {
+      id : req.params.id
+    }
+  }).then(deletedProduct => {
+    res.json(deletedProduct);
+  })
+
 });
 
 module.exports = router;
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { json } = require('sequelize/types');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
@@ -6,23 +7,60 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+
+  Tag.findAll({}).then(tags => {
+    res.json(tags);
+  })
+
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+
+  Tag.findOne({
+    where : {
+      id : req.params.id
+    }
+  }).then(tag => {
+    res.json(tag);
+  })
+
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+
+  Tag.create(req.body).then(newTag => {
+    res.json(newTag);
+  })
+
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+
+  Tag.update({
+    where : {
+      id : req.params.id
+    }
+  }).then(updatedTag => {
+    res.json(updatedTag);
+  })
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+
+  Tag.destroy({
+    where : {
+      id: req.params.id
+    }
+  }).then(deletedTag => {
+    res.json(deletedTag);
+  })
+
 });
 
 module.exports = router;
